d7f82ee5c2ddad302d9077d659c395f0fa7de15a
jaime.blanco <jaime.blanco@jalasoft.com>
2023-06-15 21:44:35 -0500

    #7 Added Routes | Description: Login and NotFound also updated test for page container

--- src/features/ui/layouts/page-container/page-container.test.tsx
+++ src/features/ui/layouts/page-container/page-container.test.tsx
@@ -1,13 +1,25 @@
 import { render, screen } from '@testing-library/react';
+import { Routes, Route, MemoryRouter } from 'react-router-dom';
+
 import { PageContainer } from '..';
+
+const TestComponent = () => (
+  <>
+    <h1>Test Component 1</h1>
+    <h2>Test Component 2</h2>
+  </>
+);
 
 describe('PageContainer', () => {
   it('should return the children passed', () => {
     render(
-      <PageContainer>
-        <h1>Test Component 1</h1>
-        <h2>Test Component 2</h2>
-      </PageContainer>
+      <MemoryRouter initialEntries={['/test']}>
+        <Routes>
+          <Route path="/test" element={<PageContainer />}>
+            <Route index element={<TestComponent />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
     );
 
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
--- src/routes/app-navigator.tsx
+++ src/routes/app-navigator.tsx
@@ -1,11 +1,11 @@
 import { ReactNode, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { LoadingScreen } from '@features/ui/loading-screen/loading-screen';
 import { PageContainer } from '@features/ui';
 import { Dashboard } from '../pages';
 
-import { PATH_DASHBOARD } from './constants';
+import { PATH_AUTH, PATH_DASHBOARD, PATH_PAGE } from './constants';
 
 // ----------------------------------------------------------------
 
@@ -15,9 +15,17 @@ const withLoading = (Component: ReactNode) => {
 
 const AppNavigator = () => (
   <Routes>
+    <Route path="/" element={<Navigate to={PATH_AUTH.login} />} />
+
+    <Route path={PATH_AUTH.login} element={<h1>Login</h1>} />
+
     <Route path={PATH_DASHBOARD.root} element={<PageContainer />}>
       <Route index element={withLoading(<Dashboard />)} />
     </Route>
+
+    <Route path={PATH_PAGE.page404} element={<h2>404 not found</h2>} />
+
+    <Route path="*" element={<Navigate to={PATH_PAGE.page404} replace />} />
   </Routes>
 );
 
