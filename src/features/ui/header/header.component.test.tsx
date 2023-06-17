1bbb7d1c7bb14ec7b8b5acfe8d6738e84531605d
jaime.blanco <jaime.blanco@jalasoft.com>
2023-06-17 16:31:35 -0500

    #10 Updated tests for header component

--- src/features/ui/header/header.component.test.tsx
+++ src/features/ui/header/header.component.test.tsx
@@ -2,10 +2,13 @@ import { render, screen } from '@testing-library/react';
 import { Header } from '.';
 
 describe('Header component', () => {
-  it('should render a button with Login text', () => {
+  it('should render a the buttons and search input', () => {
     render(<Header />);
 
     expect(screen.getByRole('button', { name: /menu/i }));
-    expect(screen.getByRole('button', { name: /login/i }));
+    expect(screen.getByRole('button', { name: /add-icon/i }));
+    expect(screen.getByRole('button', { name: /notification-icon/i }));
+    expect(screen.getByRole('button', { name: /avatar-icon/i }));
+    expect(screen.getByRole('textbox', { name: /search/i }));
   });
 });
