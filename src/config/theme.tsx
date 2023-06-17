fe32697751c5e5b759049eda59f266adcae01e45
jaime.blanco <jaime.blanco@jalasoft.com>
2023-06-17 14:45:10 -0500

    #5 Added succes, info and error colors

--- src/config/theme.tsx
+++ src/config/theme.tsx
@@ -9,6 +9,9 @@ enum ThemePallete {
   BG = '#EBEBEB',
   SURFACE = '#FFFF',
   PRIMARY_MAIN = '#F3A447',
+  SECONDARY_MAIN = '#274C77',
+  ERROR_MAIN = '#DF2935',
+  SUCCESS_MAIN = '#04E762',
   FONT_GLOBAL = 'PoppinsRegular, sans-serif',
 }
 
@@ -18,9 +21,25 @@ const theme = createTheme({
     primary: {
       main: ThemePallete.PRIMARY_MAIN,
     },
+    secondary: {
+      main: ThemePallete.SECONDARY_MAIN,
+    },
+    error: {
+      main: ThemePallete.ERROR_MAIN,
+    },
+    success: {
+      main: ThemePallete.SUCCESS_MAIN,
+    },
   },
   typography: {
     fontFamily: ThemePallete.FONT_GLOBAL,
+    h1: { fontSize: '64px', fontWeight: 'bold' },
+    h2: { fontSize: '56px', fontWeight: 'bold' },
+    h3: { fontSize: '40px', fontWeight: 'bold' },
+    h4: { fontSize: '32px', fontWeight: 'bold' },
+    h5: { fontSize: '24px', fontWeight: 'bold' },
+    body1: { fontSize: '16px' },
+    body2: { fontSize: '14px' },
   },
 });
 
